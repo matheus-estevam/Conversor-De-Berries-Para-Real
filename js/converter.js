8cc8c65f2593619bd634c3881b1d499449eaa1c0
matheus-estevam <matheusestevam17055@gmail.com>
2024-02-18 22:57:40 -0300

    update converter.js

--- js/converter.js
+++ js/converter.js
@@ -3,10 +3,10 @@ const berryInput = document.querySelector("#berry");
 const reaisInput = document.querySelector("#reais");
 
 function cambioBerry() {
-  const brl = 0.0584;
+  const brl = 0.05846666666;
   const calculo = +berryInput.value * brl;
 
-  return (reaisInput.value = `R$ ${calculo}`);
+  return (reaisInput.value = `R$ ${calculo.toFixed(2)}`);
 }
 
 botaoConverter.addEventListener("click", cambioBerry);
